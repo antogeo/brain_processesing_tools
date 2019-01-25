10cef128c09deaacaac2672fc47e1d95510e2fff
antogeo <Georgios.Antonopoulos@ulg.ac.be>
2019-01-25 22:30:32 +0100

    Update bet.py

--- bet.py
+++ bet.py
@@ -10,14 +10,14 @@ root.update()
 root.withdraw()
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 file_path = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-path, filename = os.path.split(file_path)
+f_path, filename = os.path.split(file_path)
 
 fsl.FSLCommand.set_default_output_type('NIFTI')
-example_nii = fnmatch.filter(os.listdir(path), '*.nii')
+example_nii = fnmatch.filter(os.listdir(f_path), '*.nii')
 print example_nii
-for x in range(0, len(example_nii)):
+for name in example_nii:
 	btr = fsl.BET()
-	btr.inputs.in_file = path + "/" + example_nii[x]
-	btr.inputs.out_file = path + "/res/" + "BET_7" + "_" + example_nii[x]
+	btr.inputs.in_file = os.path.join(f_path, name)
+	btr.inputs.out_file = os.path.join(f_path, res, "BET7_" + name)
 	btr.inputs.frac = 0.7
 	res = btr.run()
